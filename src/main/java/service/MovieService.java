dc44c17b305d1ce4763c431edc391ff09139e917
Satish Jaiswal <Sjsatishjaiswalcse2017@gmail.com>
2023-04-03 16:38:09 +0530

    Assignment

---
+++ src/main/java/service/MovieService.java
@@ -0,0 +1,52 @@
+package service;
+
+import com.driver.Director;
+import com.driver.Movie;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import repository.MovieRepository;
+
+import java.util.List;
+
+@Service
+public class MovieService {
+
+    @Autowired
+    MovieRepository movieRepository;
+
+    public void addMovie(Movie movie){
+        movieRepository.addMovie(movie);
+    }
+
+    public void addDirector(Director director){
+        movieRepository.addDirector(director);
+    }
+
+    public void addMovieDirectorPair(String movie,String director){
+        movieRepository.addMovieDirectorPair(movie,director);
+    }
+
+    public Movie getMovieByName(String name){
+        return movieRepository.getMovieByName(name);
+    }
+
+    public Director getDirectorByName(String name){
+        return movieRepository.getDirectorByName(name);
+    }
+
+    public List<String> getMoviesByDirectorNAme(String name){
+        return movieRepository.getMoviesByDirectorName(name);
+    }
+
+    public List<String> findAllMovies(){
+        return movieRepository.findAllMovies();
+    }
+
+    public void deleteDirectorByName(String name){
+        movieRepository.deleteDirectorByName(name);
+    }
+
+    public void deleteAllDirectors(){
+        movieRepository.deleteAllDirectors();
+    }
+}
